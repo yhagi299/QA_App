c2e7fbdf33a6547e5a7664d93fd5bbf4d5ba3ca3
yhagi299 <y_hagi29@yahoo.co.jp>
2018-03-19 19:48:20 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yasufumi/hagiwara/qa_app/Const.java
@@ -0,0 +1,13 @@
+package jp.techacademy.yasufumi.hagiwara.qa_app;
+
+/**
+ * Created by hagiwara on 2018/03/05.
+ */
+
+public class Const {
+    public static final String UserPATH = "users";
+    public static final String ContentPATH ="contents";
+    public static final String AnswersPATH = "answers";
+
+    public static final String NameKEY = "name";
+}
